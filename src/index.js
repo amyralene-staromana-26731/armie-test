1ae5992678b4fd95fbfa3ead5270eb8d97167cbc
Amyralene Sta.Romana <Amyralene.Sta.Romana@nelnet.net>
2021-07-13 09:35:47 -0500

    magic 8 ball

--- src/index.js
+++ src/index.js
@@ -1,17 +1,84 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import "./ball.css";
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const inputStyle = {
+  width: 235,
+  margin: 5
+};
+
+class Magic8Ball extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      userInput: "",
+      randomIndex: ""
+    };
+    this.ask = this.ask.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+  ask() {
+    if (this.state.userInput) {                                                                         
+      this.setState({
+        randomIndex: Math.floor(Math.random() * 20),
+        userInput: ""
+      });
+    }
+  }
+  handleChange(event) {
+    this.setState({
+      userInput: event.target.value
+    });
+  }
+  render() {
+    const possibleAnswers = [
+      "If James will dance and it's a yes!",
+      "I don't think so",
+      "Sure, Why not!",
+      "Kick Aaron and you will know the answer",
+      "You may rely on it",
+      "Margo says could be YES!",
+      "Doing Good",
+      "Si",
+      "My psychic says YES",
+      "James says Talk to my hand",
+      "James says Try Again",
+      "SECRET",
+      "Ask Mike",
+      "HOCUS FOCUS and ask again",
+      "Don't count on it",
+      "Venkata says NO",
+      "Brandon says adios",
+      "Most likely",
+      "Definitely NO",
+      "I don't think so"
+    ];
+    const answer = possibleAnswers[this.state.randomIndex];
+    return (
+      <div>
+        <div onClick={this.ask}>
+		  <h2 className={"mainTitle"}>Welcome to Magic 8 Ball </h2>
+          <div className={"titleText"}>QUESTION?</div>
+		  <br />
+          <input
+            type="text" id='question' name='question' value={this.state.userInput}
+            onChange={this.handleChange}
+            style={inputStyle}/>
+          <br /><br /><br />
+          <div className={"eightBall"}>
+            <div className={"whiteCircle"}>
+              <div className={"topEight"} />
+              <div className={"bottomEight"} />
+            </div>
+          </div>
+        </div>
+        <br />
+        <h1 className={"eightBallSays"}>{answer}</h1>
+      </div>
+    );
+  }
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<Magic8Ball />, rootElement);
